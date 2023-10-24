81a717ccf07e3abec765286a874925340c05fc3f
Vladimir Alekseev <mail@vlal.ru>
2023-10-24 12:07:23 +0300

    Donat props & data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
-import Donat from './Donat';
+import Donat, {IItem} from './Donat';
+
+const DATA: IItem[] = [
+    {
+        label: 'Управление гос. финансами',
+        value: 19611120.90,
+        color: '#3B89F0',
+    },
+    {
+        label: 'Информационное общество',
+        value: 4827629.30,
+        color: '#1C4AC1',
+    },
+    {
+        label: 'ГП НТР',
+        value: 849411.3,
+        color: '#ED8D34',
+    },
+];
 
 function App() {
   return (
     <div className="App">
-      <Donat />
+      <Donat data={DATA} size="medium" />
     </div>
   );
 }
--- src/Donat.tsx
+++ src/Donat.tsx
@@ -1,16 +1,37 @@
+import {useMemo} from "react";
 import DonutChart from 'react-donut-chart';
 
-// things I would never do:
-export default () => <DonutChart
-    data={[
-        {
-            label: 'Give you up',
-            value: 25,
-        },
-        {
-            label: '',
-            value: 75,
-            isEmpty: true,
-        },
-    ]}
-/>;
+export interface IItem {
+    label: string;
+    value: number;
+    color: string;
+}
+
+interface IDonatProps {
+    size: 'small' | 'medium' | 'large';
+    data: IItem[];
+}
+
+const Donat = ({data}: IDonatProps) => {
+    const colors = useMemo(() => data.map(item => item.color), [data]);
+
+    return <DonutChart
+        colors={colors}
+        data={[
+            {
+                label: 'Управление гос. финансами',
+                value: 19611120.90,
+            },
+            {
+                label: 'Информационное общество',
+                value: 4827629.30,
+            },
+            {
+                label: 'ГП НТР',
+                value: 849411.3,
+            },
+        ]}
+    />
+};
+
+export default Donat;
